06c7ddc8b2e6ce4f2827a6a077c3146ccf49fc9a
Gaurav <gp131028@gmail.com>
2025-01-09 15:47:57 +0530

    episode 6

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,32 @@
 import ResCards from "./ResCards";
-import resList from "../utils/mockData";
-import { useState } from "react";
+// import resList from "../utils/mockData";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
   // a local state variable
 
-  const [listOFres, setList] = useState(resList);
+  const [listOFres, setList] = useState([]);
 
-  return (
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    console.log(json);
+    setList(
+      json.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  return listOFres.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
         <button
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import { LOGO } from "../utils/constants";
 
 const Header = () => {
+  const [btnName, setName] = useState("Log-in");
+
   return (
     <div className="header">
       <div className="logo-container">
@@ -14,6 +17,16 @@ const Header = () => {
           <li>About</li>
           <li>Contact</li>
           <li>Cart</li>
+          <button
+            className="log-btn"
+            onClick={() => {
+              // const btnFetch = "logout";
+              btnName == "Log-in" ? setName("Logout") : setName("Log-in");
+              console.log(btnName);
+            }}
+          >
+            {btnName}
+          </button>
         </ul>
       </div>
     </div>
